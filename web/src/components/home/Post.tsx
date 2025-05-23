5e567a83632d8bf384b599206782eb128bf5789a
ahsanulhoqueabir <contact.ahsanul@gmail.com>
2025-05-23 22:48:11 +0600

    fix: correct layout structure in Post component for improved readability

--- web/src/components/home/Post.tsx
+++ web/src/components/home/Post.tsx
@@ -13,8 +13,8 @@ const Post: React.FC<PostProps> = ({ post }) => {
     <div className="relative post-card h-full w-full  rounded-lg shadow-lg border-[1px] shadow-gray-200 overflow-hidden p-2 ">
       <div className="lg:flex gap-3 items-center justify-between">
         <div className="grid justify-between items-stretch  w-full gap-7">
-          <div className="h-full">
-            <h1 className="textarea-md ">{post.description}</h1>
+          <div className="h-full flex flex-col justify-between">
+            <h1 className="text-md ">{post.description}</h1>
             <p className="text-xs text-gray-500 pb-2">
               <span className="text-xs">{post.courseID} </span>{" "}
             </p>
